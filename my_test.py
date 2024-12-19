9f4ae0c43a50597fad5c2b619beaa6f253b9163d
Weitao Sun <weitaos2@uci.edu>
2024-12-20 03:46:27 +0900

    LIB UPDATE np.float deprecated since 1.20, replace it with np.float64

---
+++ my_test.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+import math
+import os
+import random
+import sys
+
+import numpy as np
+import torch
+from fairseq import (
+    checkpoint_utils,
+    distributed_utils,
+    options,
+    quantization_utils,
+    tasks,
+    utils,
+)
+from fairseq.data import iterators
+from fairseq.logging import meters, metrics, progress_bar
+from fairseq.model_parallel.megatron_trainer import MegatronTrainer
+from fairseq.trainer import Trainer
